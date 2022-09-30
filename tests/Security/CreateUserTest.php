4d655e15093f9f3a5a46e5ef668f2ae8bbd40d7e
xReapex <tr7296228@gmail.com>
2022-09-30 17:09:36 +0200

    [TEST] - User Login

---
+++ tests/Security/CreateUserTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Tests\Security;
+
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class CreateUserTest extends WebTestCase
+{
+    public function testVisitingWhileLoggedIn()
+    {
+        $client = static::createClient();
+
+        $userRepository = static::$container->get(UserRepository::class);
+        $testUser = $userRepository->find(1);
+
+        $client->loginUser($testUser);
+
+        $client->request('GET', '/profile');
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', 'thomas');
+    }
+}
